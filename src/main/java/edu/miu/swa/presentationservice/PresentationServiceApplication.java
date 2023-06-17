552365aa03b145943326f2c29ba51a0e54cd4f3c
Bazz <abbasirekeola@gmail.com>
2023-06-17 12:26:03 -0500

    init

---
+++ src/main/java/edu/miu/swa/presentationservice/PresentationServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.swa.presentationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresentationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PresentationServiceApplication.class, args);
+    }
+
+}
